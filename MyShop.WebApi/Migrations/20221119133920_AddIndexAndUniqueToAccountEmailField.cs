3eaf9e0f631806c95ee119a054a84df22fd85684
word-guru <legeon48@mail.ru>
2022-11-19 17:27:51 +0300

    update

---
+++ MyShop.WebApi/Migrations/20221119133920_AddIndexAndUniqueToAccountEmailField.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MyShop.WebApi.Repository.Server.Migrations
+{
+    public partial class AddIndexAndUniqueToAccountEmailField : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Accounts_Email",
+                table: "Accounts",
+                column: "Email",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Accounts_Email",
+                table: "Accounts");
+        }
+    }
+}
