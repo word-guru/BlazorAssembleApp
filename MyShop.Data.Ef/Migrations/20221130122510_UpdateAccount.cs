af9d2b3c6dc54e8f1319b36297b947575386c357
word-guru <legeon48@mail.ru>
2022-11-30 18:03:11 +0300

    update

---
+++ MyShop.Data.Ef/Migrations/20221130122510_UpdateAccount.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MyShop.Data.Ef.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateAccount : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
